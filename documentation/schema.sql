1a5abc44ff377a75100d972e69e7b19c24618330
Emilia Segura <segura.emilia@gmail.com>
2020-09-02 21:42:25 -0400

    resized images and reorganized documentation

---
+++ documentation/schema.sql
@@ -0,0 +1,87 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
+--
+-- Host: remotemysql.com    Database: mDOrDJT6Ef
+-- ------------------------------------------------------
+-- Server version	8.0.13-4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `colours`
+--
+
+DROP TABLE IF EXISTS `colours`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `colours` (
+  `idimage` int(11) NOT NULL,
+  `colourname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idimage`,`colourname`),
+  CONSTRAINT `fk_colours_to_images` FOREIGN KEY (`idimage`) REFERENCES `images` (`idimage`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `images` (
+  `idimage` int(11) NOT NULL AUTO_INCREMENT,
+  `imageName` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `imagePath` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idimage`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `moods`
+--
+
+DROP TABLE IF EXISTS `moods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `moods` (
+  `idimage` int(11) NOT NULL,
+  `moodname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idimage`,`moodname`),
+  CONSTRAINT `fk_moods_to_images` FOREIGN KEY (`idimage`) REFERENCES `images` (`idimage`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tags` (
+  `idimage` int(11) NOT NULL,
+  `tag` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`idimage`,`tag`),
+  CONSTRAINT `fk_tag_to_image` FOREIGN KEY (`idimage`) REFERENCES `images` (`idimage`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-02 21:18:29
